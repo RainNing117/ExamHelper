b346c3add9698e62db1fce5a91f9a6afc39ed097
NameFyn117 <a18903284212@163.com>
2026-07-03 16:28:56 +0800

    原版页面切换动画

--- ExamSettings/MainWindow.xaml.cs
+++ ExamSettings/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using iNKORE.UI.WPF.Modern.Controls;
+using iNKORE.UI.WPF.Modern.Controls;
+using iNKORE.UI.WPF.Modern.Media.Animation;
 using System.Windows;
 using Page = iNKORE.UI.WPF.Modern.Controls.Page;
 
@@ -6,52 +7,32 @@ namespace ExamSettings
 {
     public partial class MainWindow : Window
     {
-        private readonly Pages.Home _pageHome;
-        private readonly Pages.ExamEditer _pageApps;
-        private readonly Pages.Settings _pageSettings;
-        private readonly Pages.About _pageAbout;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            // Pre-create page instances to improve navigation performance
-            _pageHome = new Pages.Home();
-            _pageApps = new Pages.ExamEditer();
-            _pageSettings = new Pages.Settings();
-            _pageAbout = new Pages.About();
         }
 
         private void NavigationView_Root_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
             var item = sender.SelectedItem;
-            Page? page = null;
+            Type? pageType = null;
 
             if (item == NavigationViewItem_Home)
-            {
-                page = _pageHome;
-            }
+                pageType = typeof(Pages.Home);
             else if (item == NavigationViewItem_ExamEditer)
-            {
-                page = _pageApps;
-            }
+                pageType = typeof(Pages.ExamEditer);
             else if (item == NavigationViewItem_Settings)
-            {
-                page = _pageSettings;
-            }
+                pageType = typeof(Pages.Settings);
             else if (item == NavigationViewItem_About)
-            {
-                page = _pageAbout;
-            }
+                pageType = typeof(Pages.About);
 
-            if (page != null)
+            if (pageType != null)
             {
                 NavigationView_Root.Header = (item as NavigationViewItem)?.Content;
 
-                // Avoid re-navigating to the same page instance to reduce overhead
-                if (Frame_Main.Content != page)
+                if (Frame_Main.Content?.GetType() != pageType)
                 {
-                    Frame_Main.Navigate(page);
+                    Frame_Main.Navigate(pageType, null, new SlideNavigationTransitionInfo());
                 }
             }
         }
